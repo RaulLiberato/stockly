9a1e4237f4e6992da4fd3598bc06a8b3d9464815
RaulLiberato <ctt.raul.liberato@gmail.com>
2026-04-07 16:48:47 -0300

    feat: ensure product can be delete

---
+++ app/_actions/product/delete-product/schema.ts
@@ -0,0 +1,7 @@
+import z from "zod";
+
+export const deleteProductSchema = z.object({
+  id: z.string().uuid({ message: "ID do produto inválido" }),
+});
+
+export type DeleteProductSchema = z.infer<typeof deleteProductSchema>;
---
+++ app/products/_components/delete-dialog-content.tsx
@@ -0,0 +1,47 @@
+import { deleteProduct } from "@/app/_actions/product/delete-product";
+import {
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/app/_components/ui/alert-dialog";
+import { toast } from "sonner";
+
+interface DeleteProductDialogContentProps {
+  productId: string;
+}
+
+const DeleteProductDialogContent = ({
+  productId,
+}: DeleteProductDialogContentProps) => {
+  const handleContinueClick = async () => {
+    try {
+      await deleteProduct({ id: productId });
+      toast.success("Produto deletado com sucesso!");
+    } catch (error) {
+      toast.error("Ocorreu um erro ao deletar o produto.");
+    }
+  };
+  return (
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Deletar produto</AlertDialogTitle>
+        <AlertDialogDescription>
+          Você está prestes a deletar este produto. Esta ação não pode ser
+          desfeita. Deseja continuar?
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancelar</AlertDialogCancel>
+        <AlertDialogAction onClick={handleContinueClick}>
+          Continuar
+        </AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  );
+};
+
+export default DeleteProductDialogContent;
