3a84955f6507e348aeaeecd410fadacdc5003cb0
RaulLiberato <ctt.raul.liberato@gmail.com>
2026-04-09 12:43:11 -0300

    feat: product stock is validated before adding product to sale

--- app/sales/_components/upsert-sheet-content.tsx
+++ app/sales/_components/upsert-sheet-content.tsx
@@ -79,6 +79,15 @@ const UpsertSheetContent = ({
         (product) => product.id === selectedProduct.id,
       );
       if (existingProduct) {
+        const productIsOutOfStock =
+          existingProduct.quantity + data.quantity > selectedProduct.stock;
+        if (productIsOutOfStock) {
+          form.setError("quantity", {
+            message: `Quantidade indisponível em estoque.`,
+          });
+          return currentProducts;
+        }
+        form.reset();
         return currentProducts.map((product) => {
           if (product.id === selectedProduct.id) {
             return {
@@ -89,6 +98,14 @@ const UpsertSheetContent = ({
           return product;
         });
       }
+      const productIsOutOfStock = data.quantity > selectedProduct.stock;
+      if (productIsOutOfStock) {
+        form.setError("quantity", {
+          message: "Quantidade indisponível em estoque.",
+        });
+        return currentProducts;
+      }
+      form.reset();
       return [
         ...currentProducts,
         {
